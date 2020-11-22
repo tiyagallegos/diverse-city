c12caa392e53ec318c181c71f2d967bd2886ba51
Henry Northcote <hnorthcote2000@hotmail.com>
2020-11-22 15:43:56 -0700

    Basic pages, react routing ready

---
+++ src/Pages/App/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Footer from '../../Components/Footer/Index';
+import WelcomePage from '../WelcomePage/WelcomePage';
+import BusinessPage from '../BusinessPage/BusinessPage';
+import UserDetailsPage from '../UserDetailsPage/UserDetailsPage';
+import InvestorPage from '../InverstorPage/InvestorPage';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state= {
+      user: null,
+    }
+  };
+  async compoentDidMount() {
+
+  };
+  render() {
+    return (
+  
+      <div className="App">
+          <Switch>
+              <Route exact path='/' render={props =>   
+                <WelcomePage
+                {...props}
+                />
+              }/>              <Route exact path='/business' render={props =>   
+                <BusinessPage
+                {...props}
+                />
+              }/>              <Route exact path='/user' render={props =>   
+                <UserDetailsPage
+                {...props}
+                />
+              }/>              <Route exact path='/investor' render={props =>   
+                <InvestorPage
+                {...props}
+                />
+              }/>
+          </Switch>
+        <Footer/>
+      </div>
+    );
+  };
+};
+export default App;
