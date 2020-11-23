d24276509acd06faaf3d9321da67668f546b513e
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-11-23 01:04:21 -0700

    footer path and styling changes

--- src/Components/Footer/Index.js
+++ src/Components/Footer/Index.js
@@ -2,23 +2,23 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons"
 
-//import styles from './Footer.module.scss'
+import styles from './Footer.css'
 
 export default function Footer() {
     return(
-        <footer>
+        <footer className="Footer">
             <section> Copyright &copy; {new Date().getFullYear()} </section>
             <section>
-                <a href="https://github.com/tiyagallegos/" target="_blank"> 
+                <a className="icons" href="https://github.com/tiyagallegos/" target="_blank"> 
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
-                <a href="https://github.com/hnorthcote/" target="_blank"> 
+                <a className="icons" href="https://github.com/hnorthcote/" target="_blank"> 
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
-                <a href="https://www.linkedin.com/in/tiya-gallegos/" target="_blank">
+                <a className="icons" href="https://www.linkedin.com/in/tiya-gallegos/" target="_blank">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
-                <a href="https://www.linkedin.com/in/henry-northcote/" target="_blank">
+                <a className="icons" href="https://www.linkedin.com/in/henry-northcote/" target="_blank">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
                
